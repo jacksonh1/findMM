7fe0099912958cf7b67d829c0cfbbe551a0e8a12
jacksonh1 <23747834+jacksonh1@users.noreply.github.com>
2018-11-06 16:22:01 -0500

    adding argument parser

--- development/argparse_test2.py
+++ development/argparse_test2.py
@@ -13,13 +13,18 @@
     - k-mer reads go here
     - table and network go here
     - default: cwd
+
+Any mutually exclusive groups?
 '''
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='identify multi-mapping transcripts and their connectivity in a reference transcriptome fasta file')
-parser.add_argument('-k', metavar='integer', nargs=1, type=int, help='k-mer read lengths (i.e. k). Recommend k be equal to the smallest read length in an experimental dataset')
-parser.add_argument('bowtie_path', default='bowtie', help='the path to the bowtie executable. Default is bowtie in PATH')
+parser = argparse.ArgumentParser(
+    description='identify multi-mapping transcripts and their connectivity in a reference transcriptome fasta file')
+parser.add_argument('-k', metavar='integer', nargs=1, type=int,
+                    help='k-mer read lengths (i.e. k). Recommend k be equal to the smallest read length in an experimental dataset')
+parser.add_argument('-b', '--bowtie_path', default='bowtie',
+                    help='the path to the bowtie executable. Default is bowtie in PATH')
 args = parser.parse_args()
 
-print(args.k,args)
+print(args.k, args)
